b06c0ba613843ef429b9aa98230665df5993e9dc
kimjunwoo <128487020+kimjunwooo@users.noreply.github.com>
2023-11-15 19:35:17 +0900

    Title: 큰 숫자 자리수의 합, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
+++ 231115/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
@@ -1,5 +1,5 @@
 a, b, c = map(int,input().split())
-n = a * b * c
+
 
 def sum_digit(n):
     if n < 10:
@@ -7,4 +7,4 @@ def sum_digit(n):
     
     return sum_digit((n-1) // 10) + n % 10
 
-print(sum_digit(n))
+print(sum_digit(a * b * c))
