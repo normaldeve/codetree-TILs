35d7f838d2c4645bdbdef1f96b175e162cba0363
kimjunwoo <128487020+kimjunwooo@users.noreply.github.com>
2023-11-15 22:32:12 +0900

    Title: 정수의 최솟값, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231115/정수의 최솟값/minimum-value-of-an-integer.py
+++ 231115/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,9 +1,11 @@
 arr = list(map(int,input().split()))
-
-def find_min(arr):
-    min_ = 0
-    for i in arr:
-        min_ = min(min_,i)
-    return min_
-
-print(find_min(arr))
+a, b, c = arr[0], arr[1], arr[2]
+def find_min(a,b,c):
+    min_val = a
+    if min_val > b:
+        min_val = b
+    if min_val > c:
+        min_val = c
+    
+    return min_val
+print(find_min(a,b,c))
