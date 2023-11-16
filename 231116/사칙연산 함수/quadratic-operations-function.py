7b4103832e8b2c4f9da3b34a3d4c01e681fbceb0
kimjunwoo <128487020+kimjunwooo@users.noreply.github.com>
2023-11-16 20:05:42 +0900

    Title: 사칙연산 함수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231116/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,16 @@
+arr = list(input().split())
+val1, opt, val2 = int(arr[0]), arr[1], int(arr[2])
+
+def operator(val1,opt,val2):
+    if opt == '+':
+        return val1 + val2
+    if opt == '-':
+        return val1 - val2
+    if opt == '/':
+        return int(val1 / val2)
+    if opt == '*':
+        return val1 * val2
+    
+    return False
+
+print(f'{val1} {opt} {val2} = {operator(val1,opt,val2)}')
