bda959d604b439aaadc3f5463b0af5b0af933655
kimjunwoo <128487020+kimjunwooo@users.noreply.github.com>
2023-11-17 23:37:48 +0900

    Title: palindrome 여부 판단하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 231117/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 231117/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -2,11 +2,11 @@ arr = input()
 
 def palindrome(arr):
     for i in range(len(arr)):
-        if arr[i] == arr[len(arr)-1-i]:
-            return True
+        if arr[i] != arr[len(arr)-1-i]:
+            return False
         
     else:
-        return False
+        return True
 
 if palindrome(arr) == True:
     print('Yes')
