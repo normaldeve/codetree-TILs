69105467aadb14d727f9dc3f8dae6dae479e936c
kimjunwoo <128487020+kimjunwooo@users.noreply.github.com>
2023-11-19 21:33:33 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231119/피보나치 수/fibonacci-number.py
+++ 231119/피보나치 수/fibonacci-number.py
@@ -1,10 +1,15 @@
 # 피보나치 수 구하기
 n = int(input())
 
+memo = [-1] * (n+1)
 def f(n):
+    if memo[n] != -1:
+        return memo[n]
     if n <= 2:
         return 1
     else:
-         return f(n-1) + f(n-2)
+        memo[n] = fibbo(n-1) + fibbo(n-2)
+
+    return memo[n]
 
 print(f(n))
