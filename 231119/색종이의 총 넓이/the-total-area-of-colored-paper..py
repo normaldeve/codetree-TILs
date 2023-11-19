fec9fe5276f3082b17edcf6cde111a2a8aa0b814
kimjunwoo <128487020+kimjunwooo@users.noreply.github.com>
2023-11-19 20:33:41 +0900

    Title: 색종이의 총 넓이, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231119/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -0,0 +1,32 @@
+OFFSET = 100
+MAX_R = 200
+
+n = int(input())
+
+rects = [
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
+
+checked = [
+    [0] * (MAX_R + 1)
+    for _ in range(MAX_R + 1)
+]
+
+for x1, y1 in rects:
+    x2, y2 = x1 + 8, y1 + 8
+
+    x1, y1 = x1 + OFFSET, y1+ OFFSET
+    x2,y2 = x2 + OFFSET, y2 + OFFSET
+
+    for x in range(x1,x2):
+        for y in range(y1,y2):
+            checked[x][y] += 1
+        
+area = 0
+for x in range(MAX_R + 1):
+    for y in range(MAX_R + 1):
+        if checked[x][y]:
+            area += 1
+
+print(area)
