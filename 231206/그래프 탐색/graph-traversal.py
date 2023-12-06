ee40f8d10e48f66a4741d769f81fe7da88e48f74
kimjunwoo <128487020+kimjunwooo@users.noreply.github.com>
2023-12-06 17:53:46 +0900

    Title: 그래프 탐색, Time: 158ms, Memory: 26MB, Status: Passed - CodeTree

--- 231206/그래프 탐색/graph-traversal.py
+++ 231206/그래프 탐색/graph-traversal.py
@@ -1,15 +1,34 @@
-arr = tuple(map(int,input().split()))
-VERTEX_NUM, n = arr[0], arr[1]
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 
-graph = [[] for _ in range(VERTEX_NUM+1)]
+#index를 1번 부터 사용하기 위해 m+1만큼 할당합니다.
+graph = [[] for _ in range(n + 1)]
 
-vertex = [
-    tuple(map(int,input().split()))
-    for _ in range(n)
-    ]
+visited = [False for _ in range(n + 1)]
+vertex_cnt = 0
 
-for start, end in vertex:
-    graph[start].append(end)
-    graph[end].append(start)
 
-print(len(graph[1]))
+def dfs(vertex):
+    global vertex_cnt
+    
+    # 해당 정점에서 이어져있는 모든 정점을 탐색해줍니다.
+    for curr_v in graph[vertex]:
+        # 아직 간선이 존재하고 방문한 적이 없는 정점에 대해서만 탐색을 진행합니다.
+        if not visited[curr_v]:
+            visited[curr_v] = True
+            vertex_cnt += 1
+            dfs(curr_v)
+    
+    
+for i in range(m):
+    v1, v2 = tuple(map(int, input().split()))
+
+    # 각 정점이 서로 이동이 가능한 양방향 그래프이기 때문에
+    # 각 정점에 대한 간선을 각각 저장해줍니다.
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+visited[1] = True
+dfs(1)
+
+print(vertex_cnt)
