60827df0e7329630b3dd39d0c514a4c719ebac5f
kimjunwoo <128487020+normaldeve@users.noreply.github.com>
2024-08-17 17:46:31 +0900

    Title: 원형 순열에서의 인원 제거, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240817/원형 순열에서의 인원 제거/josephus-permutations.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static int n, k;
+    public static Queue<Integer> stack = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            stack.add(i);
+        }
+
+        while (stack.size() != 0) {
+            for (int i = 0; i < k - 1; i++) {
+                stack.add(stack.poll());
+            }
+            System.out.print(stack.poll());
+        }
+
+
+    }
+}
