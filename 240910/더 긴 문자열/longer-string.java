7b2b7657358c4cbfe8eea65bfc4b36c6f292ccd8
kimjunwoo <128487020+normaldeve@users.noreply.github.com>
2024-09-10 23:37:42 +0900

    Title: 더 긴 문자열, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240910/더 긴 문자열/longer-string.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+
+        if (str1.length() > str2.length()) {
+            System.out.println(str1 + " " + str1.length());
+        } else {
+            System.out.println(str2 + " " + str2.length());
+        }
+    }
+}
