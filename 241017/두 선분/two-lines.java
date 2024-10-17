14b7d683e9c2c9ede1619a9c16565d36ec0d23e5
kimjunwoo <128487020+normaldeve@users.noreply.github.com>
2024-10-17 16:54:06 +0900

    Title: 두 선분, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241017/두 선분/two-lines.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+        boolean isIntersect = false;
+
+        int[] graph = new int[100];
+        for(int i = x1; i <= x2; i++) {
+            graph[i] = 1;
+        }
+        for(int i = x3; i <= x4; i++) {
+            if (graph[i] == 1) {
+                isIntersect = true;
+                break;
+            } else {
+                graph[i] = 1;
+            }
+        }
+        if (isIntersect) {
+            System.out.print("intersecting");
+        } else {
+            System.out.print("nonintersecting");
+        }
+    }
+}
