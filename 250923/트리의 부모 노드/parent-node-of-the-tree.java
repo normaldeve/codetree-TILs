33f406df751d770dda9ffbccefab555c13ec755c
김준우 <128487020+normaldeve@users.noreply.github.com>
2025-09-23 23:32:48 +0900

    Title: 트리의 부모 노드, Time: 1839ms, Memory: 67MB, Status: Passed - Codetree

--- 250923/트리의 부모 노드/parent-node-of-the-tree.java
+++ 250923/트리의 부모 노드/parent-node-of-the-tree.java
@@ -1,27 +1,36 @@
-import java.util.Scanner;
+
+import java.util.*;
 public class Main {
-    static final int MAX_NUMBER = 100000
-    static int[] visited = new int[MAX_NUMBER + 1];
-    static int[] parents = new int[MAX_NUMBER + 1];
-    static List<Integer>[] edges new ArrayList<>[MAX_NUMBER + 1];
+    static int n;
+    static final int MAX_NUMBER = 100000;
+    static boolean[] visited = new boolean[MAX_NUMBER + 1];
+    static int[] parent = new int[MAX_NUMBER + 1];
+    static List<Integer>[] edges = new ArrayList[MAX_NUMBER + 1];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n - 1; i++) {
+        // 입력:
+        n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++)
+            edges[i] = new ArrayList<>();
+
+        // n - 1개의 간선 정보를 입력받습니다.
+        for(int i = 1; i<= n - 1; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-
+            
+            // 간선 정보를 인접리스트에 넣어줍니다.
             edges[x].add(y);
             edges[y].add(x);
         }
 
+        // 1번부터 트리 순회를 진행합니다.
         visited[1] = true;
         dfs(1);
 
-        for(int i = 2; i <= n; i++) {
-            System.out.println(parents[i]);
-        }
-        
+        // 부모 노드를 출력합니다.
+        for(int i = 2; i <= n; i++)
+            System.out.println(parent[i]);
     }
 
     static void dfs(int x) {
@@ -29,7 +38,7 @@ public class Main {
             int y = edges[x].get(i);
             if (!visited[y]) {
                 visited[y] = true;
-                parents[y] = x;
+                parent[y] = x;
 
                 dfs(y);
             }
