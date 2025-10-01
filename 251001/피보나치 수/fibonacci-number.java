ea6ddc007f50db468952bc6e6dda7326f4ea21e1
김준우 <128487020+normaldeve@users.noreply.github.com>
2025-10-01 14:41:01 +0900

    Title: 피보나치 수, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251001/피보나치 수/fibonacci-number.java
+++ 251001/피보나치 수/fibonacci-number.java
@@ -3,15 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] memo = new int[n + 1];
+        int[] memo = new int[n + 2];
 
         memo[1] = 1;
         memo[2] = 1;
-
-        if (n <= 2) {
-            System.out.println(memo[n]);
-            return;
-        }
 
         for(int i = 3; i <= n; i++) {
             memo[i] = memo[i - 1] + memo[i - 2];
